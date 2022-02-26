253f1d4c8f637c3f7741431fe57346455e2f29cc
Górecki Krzysztof <goreckik@indykpol.pl>
2022-02-26 13:31:52 +0100

    zakończenie zajęć

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 
 import sys
+import math
 
 print(sys.version)
 
@@ -19,3 +20,13 @@ print(type(d))
 e=3+2j
 print(e)
 print(type(e))
+
+f= 7
+g = 3
+print(f//g) #dzielenie całkowite
+
+print(f**g) #potegowanie
+print(math.pow(f,g)) #potegowanie za pomocą biblioteki math
+
+print(2/3**g) #potegowanie
+print(math.pow(2/3,g)) #potegowanie za pomocą biblioteki math
