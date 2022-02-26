9057cc7160278c2a11e73e74d7de9f9659392bb4
Górecki Krzysztof <goreckik@indykpol.pl>
2022-02-26 13:22:57 +0100

    typy danych

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ import sys
 print(sys.version)
 
 a=15
+b='wizualizacja danych'
+print(b)
+print(type(b))
+c=15.5
+print(c)
+d= 15, 5
+print(d)
+print(type(c))
+print(type(d))
+e=3+2j
+print(e)
+print(type(e))
